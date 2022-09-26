37f5352c08c6401e3d7b052964159dbeef1664f4
disco <discodamone@wearehackerone.com>
2022-09-26 02:07:15 -0400

    Added user info functionality that retrieves data from github's API

--- pages/api/user/info.js
+++ pages/api/user/info.js
@@ -4,6 +4,16 @@ var http = require('axios');
 export default async function handler(req, res) 
 {
 	var session = await getSession(req, res);
-	//var response = await http.get('https://api.github.com/user',{headers:{Authorization: 'Bearer ' + session.oauth_token}});
-	res.status(200).json(session);
+	var userInfoResponse = await http.get('https://api.github.com/user',{headers:{Authorization: 'Bearer ' + session.oauth_token}});
+	var emailResponse = await http.get('https://api.github.com/user/emails',{headers:{Authorization: 'Bearer ' + session.oauth_token}});
+	var primaryEmail;
+	for (var i = 0; i < emailResponse.data.length; i++)
+	{
+		if (emailResponse.data[i].primary == true)
+		{
+			primaryEmail = emailResponse.data[i].email;
+			break;
+		}
+	}
+	res.status(200).json({userId: userInfoResponse.data.id, username: userInfoResponse.data.login, email: primaryEmail});
 }
